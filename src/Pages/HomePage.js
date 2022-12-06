dbce3ce85a8e98b9218152546913a4b9df00898a
Tanndlin <tanndlin@gmail.com>
2022-12-05 21:43:01 -0500

    Set up

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Components/Header';
+import MainContainer from '../Components/MainContainer';
+
+const HomePage = () => {
+    return (
+        <>
+            <Header />
+            <MainContainer />
+        </>
+    );
+};
+
+export default HomePage;
