b5e74ee6dc94922c8dd91444baab1c325d6b54c2
Tanndlin <tanndlin@gmail.com>
2022-12-07 00:30:08 -0500

    Name Website

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header className="text-4xl font-bold flex text-center bg-sky-900 text-white h-16">
-            <h1 className="m-auto">Calculator</h1>
+            <h1 className="m-auto">DynamiCalc</h1>
         </header>
     );
 };
