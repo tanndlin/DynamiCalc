c945819316195a58a31af57bf0b11542e60a624b
Tanner Sandlin <tanner.sandlin@gtri.gatech.edu>
2022-12-07 11:48:21 -0500

    Sort Vars

--- src/Components/VariableViewer.js
+++ src/Components/VariableViewer.js
@@ -6,16 +6,18 @@ const VariableViewer = (props) => {
     return (
         <section className="px-4 flex flex-col gap-4">
             <h1 className="text-center text-xl font-bold">Variables</h1>
-            {Object.keys(props.vars).map((key) => (
-                <Variable
-                    key={key}
-                    name={key}
-                    value={vars[key]}
-                    isStatic={isStatic}
-                    evaluate={props.evaluate}
-                    delete={() => props.delete(key)}
-                />
-            ))}
+            {Object.keys(props.vars)
+                .sort()
+                .map((key) => (
+                    <Variable
+                        key={key}
+                        name={key}
+                        value={vars[key]}
+                        isStatic={isStatic}
+                        evaluate={props.evaluate}
+                        delete={() => props.delete(key)}
+                    />
+                ))}
         </section>
     );
 };
