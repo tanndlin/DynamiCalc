4e020b811c780782b906590181934e2885c02f1c
Tanner Sandlin <tanner.sandlin@gtri.gatech.edu>
2022-12-07 11:54:13 -0500

    Rename folders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePage from './Pages/HomePage';
+import HomePage from './pages/HomePage';
 import './App.css';
 
 function App() {
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from '../Components/Header';
-import MainContainer from '../Components/MainContainer';
+import Header from '../components/Header';
+import MainContainer from '../components/MainContainer';
 
 const HomePage = () => {
     return (
