ece2550650a75e20fd1b770e466eeffa28efbc1e
Tanner Sandlin <tanner.sandlin@gtri.gatech.edu>
2022-12-12 12:16:12 -0500

    Draw Ticks

--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -12,16 +12,9 @@ const Graph = (props) => {
         return Math.eval(withVars);
     };
 
-    const canvasRef = React.useRef(null);
-    React.useEffect(() => {
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext('2d');
-
+    const drawAxes = (ctx, xScale, yScale) => {
         ctx.fillStyle = 'white';
         ctx.fillRect(0, 0, width, height);
-
-        const xScale = 100;
-        const yScale = 100;
 
         ctx.strokeStyle = 'black';
         ctx.beginPath();
@@ -30,6 +23,57 @@ const Graph = (props) => {
         ctx.moveTo(width / 2, 0);
         ctx.lineTo(width / 2, height);
         ctx.stroke();
+
+        // Draw ticks
+        ctx.beginPath();
+        for (let i = 0; i < width; i += xScale) {
+            ctx.moveTo(i, height / 2 - 5);
+            ctx.lineTo(i, height / 2 + 5);
+        }
+        for (let i = 0; i < height; i += yScale) {
+            ctx.moveTo(width / 2 - 5, i);
+            ctx.lineTo(width / 2 + 5, i);
+        }
+        ctx.stroke();
+
+        // Draw labels
+        ctx.fillStyle = 'black';
+        ctx.font = '15px Arial';
+
+        // Draw 0
+        ctx.fillText('0', width / 2 + 5, height / 2 + 20);
+
+        // Right
+        for (let i = width / 2 + xScale; i < width; i += xScale) {
+            ctx.fillText((i - width / 2) / xScale, i + 16, height / 2 + 20);
+        }
+
+        // Left
+        for (let i = width / 2 - xScale; i > 0; i -= xScale) {
+            ctx.fillText((i - width / 2) / xScale, i + 12, height / 2 + 20);
+        }
+
+        // Down
+        for (let i = height / 2 + yScale; i < height; i += yScale) {
+            ctx.fillText((height / 2 - i) / yScale, width / 2 - 20, i + 3);
+        }
+
+        // Up
+        for (let i = height / 2 - yScale; i > 0; i -= yScale) {
+            ctx.fillText((height / 2 - i) / yScale, width / 2 - 20, i + 3);
+        }
+
+        ctx.stroke();
+    };
+
+    const canvasRef = React.useRef(null);
+    React.useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+
+        const xScale = 100;
+        const yScale = 100;
+        drawAxes(ctx, xScale, yScale);
 
         functions.forEach((fn) => {
             try {
