e364787ddfc8e77124978c5d56d9e98a1ec2686f
Tanner Sandlin <tanner.sandlin@gtri.gatech.edu>
2022-12-12 13:44:57 -0500

    Color Pickers???

--- src/Components/FunctionViewer.js
+++ src/Components/FunctionViewer.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { CompactPicker } from 'react-color';
 
 const FunctionViewer = (props) => {
     const { fn, edit } = props;
+    const { f, color } = fn;
 
     const onEdit = (e) => {
         const newFn = { ...fn };
@@ -9,8 +11,14 @@ const FunctionViewer = (props) => {
         edit(newFn);
     };
 
+    const editColor = (color) => {
+        const newFn = { ...fn };
+        newFn.color = color.hex;
+        edit(newFn);
+    };
+
     return (
-        <div className="relative function shadow-tertiary shadow-md p-4 w-full rounded-md">
+        <div className="relative function shadow-tertiary shadow-md p-4 w-full rounded-md flex flex-col">
             <input
                 className="close"
                 type="button"
@@ -20,9 +28,13 @@ const FunctionViewer = (props) => {
             <input
                 className="text-black w-9/10 px-2 py-1 rounded-md"
                 type="text"
-                value={fn.f}
+                value={f}
                 onChange={onEdit}
             />
+
+            <footer className="picker">
+                <CompactPicker color={color} onChange={editColor} />
+            </footer>
         </div>
     );
 };
